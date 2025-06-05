605b55f5b84b45e3a52544082be5d8d41503edea
Hare21 <hareddy21@gmail.com>
2025-06-04 22:29:18 -0500

    Resolving Cors issue

--- src/main/java/com/example/RemindMeApp/LoginController.java
+++ src/main/java/com/example/RemindMeApp/LoginController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
 
